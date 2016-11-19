dae02fffccf1dab16f4ad87a80394b7effc96815
molchanovef <molchanov@tvhelp.ru>
2016-11-19 18:35:30 +0300

    some changes in sources

--- 1.5/charcount.c
+++ 1.5/charcount.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-//#define WHILE
+	#define WHILE
+
+void usage(void)
+{
+	printf("\nThis program count number of entered characters\n");
+	printf("For stop characters enter and get result press CTRL+D\n");
+}
+
 main()
 {
+	usage();
 #ifdef WHILE	
 	long nc;
 	nc = 0;
+	printf(">");
 	while(getchar() != EOF)
 		++nc;
-	printf("%ld\n",nc);
+	printf("\nYou have entered %ld characters\n",nc);
 #else
 /*
  В printf спецификатор %f применяется как для float, так и для double; спецификатор %.0f означает печать без десятичной точки и дробной части (последняя в нашем случае отсутствует).
@@ -15,8 +24,9 @@ main()
 Тело указанного for-цикла пусто, поскольку кроме проверок и приращений счетчика делать ничего не нужно. Но правила грамматики Си требуют, чтобы for- цикл имел тело. Выполнение этого требования обеспечивает изолированная точка с запятой, называемая пустой инструкцией. Мы поставили точку с запятой на отдельной строке для большей наглядности.
 */
 	double nc;
+	printf(">");
 	for(nc = 0; getchar() != EOF; ++nc)
 		;
-	printf("%.0f\n",nc);
+	printf("\nYou have entered %.0f characters\n",nc);
 #endif	
 }
--- 1.5/linecount.c
+++ 1.5/linecount.c
@@ -3,10 +3,18 @@
 Подсчет строк входного потка
 */
 
+void usage(void)
+{
+	printf("\nThis program count number of lines, tabs and spaces\n");
+	printf("For stop characters enter and get result press CTRL+D\n");
+}
+
 main()
 {
 	int c, nl, space, tab;
+	usage();
 	nl = tab = space = 0;
+	printf(">");
 	while((c = getchar()) != EOF) {
 		if(c == '\n')
 			++nl;
